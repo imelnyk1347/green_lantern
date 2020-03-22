a9d82398b3da617cd99789e3d94d38c4980876de
imelnyk1347 <47279260+imelnyk1347@users.noreply.github.com>
2020-03-22 21:45:30 +0200

    Update homework.py
    add fix for run

--- lantern/oop/homework/homework.py
+++ lantern/oop/homework/homework.py
@@ -83,20 +83,24 @@ class Cat:
 
     def run(self, hours):
 
-        if hours * self.average_speed <= 25:
+        cat_run = hours * self.average_speed
+
+        if cat_run <= 25:
             self.saturation_level = self._reduce_saturation_level(2)
 
-        if 26 <= hours * self._set_average_speed <= 50:
+        if 26 <= cat_run <= 50:
             self.saturation_level = self._reduce_saturation_level(5)
 
-        if 51 <= hours * self.average_speed <= 100:
+        if 51 <= cat_run <= 100:
             self.saturation_level = self._reduce_saturation_level(15)
 
-        if 101 <= hours * self.average_speed <= 200:
+        if 101 <= cat_run <= 200:
             self.saturation_level = self._reduce_saturation_level(25)
 
-        if hours * self.average_speed >= 201:
+        if cat_run >= 201:
             self.saturation_level = self._reduce_saturation_level(50)
+
+        return f"Your cat ran {cat_run} kilometers"
 
     def get_saturation_level(self):
 
