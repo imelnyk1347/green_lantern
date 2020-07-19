3924ad419df94640a935a47fbf7bc419c0c82333
imelnyk1347 <imelnyk1347@gmail.com>
2020-07-19 16:58:47 +0300

    Create migrations

--- lantern/flask/flask_alchemy_docker/grocery_store/config.py
+++ lantern/flask/flask_alchemy_docker/grocery_store/config.py
@@ -5,6 +5,8 @@ FIXTURES_DIR = os.path.join(ROOT_DIR, "fixtures")
 
 
 class Config:
+    HOST = "localhost"
+    PORT = 5000
     PG_USER = "cursor"
     PG_PASSWORD = "very_secret_password"
     PG_HOST = "localhost"
