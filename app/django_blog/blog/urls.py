4114224159ffa6824b05d212391ed2347fc1c203
imelnyk1347 <imelnyk1347@gmail.com>
2020-08-23 16:34:14 +0300

    Create models, post_detail view, add slug url

--- app/django_blog/blog/urls.py
+++ app/django_blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import posts_list
+from .views import posts_list, post_detail
 
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url')
 ]
