dce3958153095e1c4911066ef021fb1221e2ca48
imelnyk1347 <imelnyk1347@gmail.com>
2020-08-25 23:18:16 +0300

    Add include file, tag models, tags cards

--- app/django_blog/blog/urls.py
+++ app/django_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import posts_list, post_detail
+from .views import posts_list, post_detail, tags_list, tag_detail
 
 
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url')
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_detail_url'),
+    path('tags/<str:slug>', tag_detail, name='tag_detail_list_url')
 ]
